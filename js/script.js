a1d70d7e9eedbc6710c2a9d95e68b05c244d210e
Niccolo Rojas <niccolo.rojas.ge@gmail.com>
2025-12-12 16:10:33 +0100

    numeri casuali

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// seleziono tutti i campi
+const timer = document.getElementById("coountdown");
+const numbersList = document.getElementById("number-list");
+const buttonConfirm = document.querySelector("btn-primary");
+const messageDanger = document.querySelector("text-danger");
+const selectionNumber = document.getElementById("answers-form");
+
+//far visualizzare al utente 5 numeri casuali
+function genRandomNumInRange(0, 50) {
+    return Math.floor(Math.random() * (0 - 50 + 1)) + min;
+};
+
+
+//fai partire un timer di 30 secondi 
+setTimeout (myfunction , 1000);
+
+let seconds = 2 seconds;
+
+timer.innerText = seconds;
+
+const in
+
+
+
+
+
+
+//far apparire 5 input in cui l utente deve inserire i 5 numeri 
+
+
+
+
+//software che dice quanti e quali numeri ha ricordato
