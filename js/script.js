98fd2f0846da5b801a2aa5830f5615fe7ea8000b
Niccolo Rojas <niccolo.rojas.ge@gmail.com>
2025-12-12 17:46:02 +0100

    whille

--- js/script.js
+++ js/script.js
@@ -6,19 +6,28 @@ const messageDanger = document.querySelector("text-danger");
 const selectionNumber = document.getElementById("answers-form");
 
 //far visualizzare al utente 5 numeri casuali
-let = Result;
-
-for (i = 0, let, i < 5, i++) {
-    function getRandomIntInclusive(min, max) {
-        const minCeiled = Math.ceil(min);
-        const maxFloored = Math.floor(max);
-        return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); // The maximum is inclusive and the minimum is inclusive
-    }
-}
-console.log(getRandomIntInclusive(0, 50));
-
 numbersList.innerText = getRandomIntInclusive(0, 50);
-numbersList.innerText = Result
+ 
+const numArr = [] 
+
+while (numArr.length < 5 ) {
+   const randomNum = getRandomIntInclusive (0,50);
+if  (!numArr.includes (randomNum)){
+numArr.push(randomNum);
+}
+}
+
+function getRandomIntInclusive(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); 
+}
+    
+
+
+
+
+
 
 //fai partire un timer di 30 secondi 
 
@@ -34,10 +43,6 @@ const tims = setInterval(() => {
         timer.innerText = seconds
     }
 }, 1000)
-
-
-
-
 
 
 //far apparire 5 input in cui l utente deve inserire i 5 numeri 
