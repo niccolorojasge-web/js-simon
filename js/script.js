f27c79a703bf21bc179485f13291de4731d0a9d8
Niccolo Rojas <niccolo.rojas.ge@gmail.com>
2025-12-12 18:04:20 +0100

    push array

--- js/script.js
+++ js/script.js
@@ -6,27 +6,21 @@ const messageDanger = document.querySelector("text-danger");
 const selectionNumber = document.getElementById("answers-form");
 
 //far visualizzare al utente 5 numeri casuali
-numbersList.innerText = getRandomIntInclusive(0, 50);
  
-const numArr = [] 
 
+function getRandomIntInclusive(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); }
+
+  const numArr = []
 while (numArr.length < 5 ) {
    const randomNum = getRandomIntInclusive (0,50);
 if  (!numArr.includes (randomNum)){
 numArr.push(randomNum);
 }
 }
-
-function getRandomIntInclusive(min, max) {
-    const minCeiled = Math.ceil(min);
-    const maxFloored = Math.floor(max);
-    return Math.floor(Math.random() * (maxFloored - minCeiled + 1) + minCeiled); 
-}
-    
-
-
-
-
+numbersList.innerText = numArr ;
 
 
 //fai partire un timer di 30 secondi 
